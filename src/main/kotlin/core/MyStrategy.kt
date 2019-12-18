cc028e8f591261cff5e0eb7021a4c943e6ed8968
Karloid <krldboyan@gmail.com>
2019-12-18 11:14:55 +0300

    Update MyStrategy.kt

--- src/main/kotlin/core/MyStrategy.kt
+++ src/main/kotlin/core/MyStrategy.kt
@@ -14,6 +14,7 @@ import java.util.*
 //TODO Handle two units at once
 //TODO highier score to keep jumping
 //TODO calc trampline
+//TODO better resource management, keep to center of the map
 class MyStrategy : AbstractStrategy() {
 
     private var prevGame: Game? = null
