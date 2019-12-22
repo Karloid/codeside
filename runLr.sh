0d5635c6e864c703ecaa901bb8af7b9991d57eca
dmitry <my@my.ru>
2019-12-22 03:09:54 +0300

    - b 16835 (round2_2)

--- runLr.sh
+++ runLr.sh
@@ -4,7 +4,8 @@
 #./kill.sh || true && echo 'startLr' && cd ./lr && ./aicup2019 --config config_level.json &
 sleep 2
 #java -jar ./strategy13379.jar 127.0.0.1 31002 &
-java -jar ./strategy16735.jar 127.0.0.1 31002 &
+java -jar ./strategy16835.jar 127.0.0.1 31002 &
+#java -jar ./strategy16735.jar 127.0.0.1 31002 &
 #java -jar ./strategy812.jar 127.0.0.1 31002 &
 #./kill.sh || true && echo 'startLr' && cd ./lr && ./aicup2019 --config config_keyboard.txt & sleep 2
 #./kill.sh || true && echo 'startLr' && cd ./lr && ./aicup2019 --config config_level.json & sleep 2
